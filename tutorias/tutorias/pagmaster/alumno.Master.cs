b81748d2f221cae148159ac35b401458cc8f7e19
walle97 <jorgev537@gmail.com>
2020-01-23 15:34:47 -0600

    se agrego pagina maestra

---
+++ tutorias/tutorias/pagmaster/alumno.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace tutorias.pagmaster
+{
+    public partial class alumno : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
